1cef93cf5e0557cef922ca0082140f87fd2fd9d0
Innovator <shahriar.firoz@gmail.com>
2018-09-20 03:26:26 -0500

    Second Commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,9 @@
-var totalCorrect = 0;
+var correctNum = 0;
 var incorrectNum = 0;
 var unAnswered = 0;
 var counter = 0;
 var time;
-var timerCounter = 30;
+var timerStart = 30;
 
 var questionsArray = [
     "How many presents did Dudley originally get on his birthday before screaming at Petunia for more?",
@@ -42,28 +42,134 @@ var correctArray = [
     "Fleur, Krum, Cedric, Harry"
 ];
 
+var Q = ["A.","B.","C.","D."]
+
 
 
 $("#startGame").on("click", function(event) {
     $("#startGame").hide();
     
-    timeCount();
-    generateHTML();
+    countDown();
+    showQuestion();
 
 });
 
-function timeCount() {
+function countDown() {
     time = setInterval(thirty, 1000);
 
     function thirty() {
-        if (timerCounter === 0) {
+        if (timerStart === 0) {
             clearInterval(time);
-            questionTimeout();
+            timeout();
         }
-        else if (timerCounter > 0) {
-            timerCounter--;
+        else if (timerStart > 0) {
+            timerStart--;
         }
-        $(".timer").html(timerCounter);
+        $(".timer").html(timerStart);
     }
-}
+};
 
+// var gameContent = "<h2>Time Remaining: <span class='timer'>" + timerStart + "</span></h2>";
+
+function contentBuild(){
+    $(".gameDiv").empty();
+
+    $(".gameDiv").append($("<h2 id='timeR'>"));
+    $("<h2>").addClass("text-center timer-p");
+    $("#timeR").append($("<span class='timer'>"));
+    $(".timer").html(timerStart);
+};
+
+function showQuestion(){
+    console.log(counter);
+    contentBuild();
+
+    $(".gameDiv").append($("<h3 class='text-center'>" + questionsArray[counter] + "</h3>"));
+
+    for (var i=0; i < 4; i++){
+    $(".gameDiv").append($("<h3 class='answer'>"+Q[i]+" "+ answersArray[counter][i] + "</h3>"));
+    }
+    console.log(counter);
+
+};
+
+function timeout(){
+    unAnswered++
+
+    
+    contentBuild();
+
+    $(".gameDiv").append($("<h3 class='text-center'> You ran out of time!</h3>"));
+    $(".gameDiv").append($("<h3>The correct answer is: "+correctArray[counter] + "</h3>"));
+
+    setTimeout(pause, 2000);
+};
+
+$("body").on("click", ".answer", function(event){
+
+    var userChoice = $(this).text();
+    var userAnswer = userChoice.substring(userChoice.indexOf(".")+2);
+    console.log(userChoice);
+    console.log(correctArray[counter]);
+    console.log(userAnswer);
+
+    if (userAnswer === correctArray[counter]){
+        showCorrect();
+    } else {
+        showIncorrect();
+    }
+    clearInterval(time)
+
+});
+
+
+function showCorrect(){
+    correctNum++;
+    contentBuild();
+
+    $(".gameDiv").append($("<h3 class='text-center'>Correct! The answer is: "+correctArray[counter]+"</h3>"));
+
+    setTimeout(pause, 2000);
+};
+
+function showIncorrect(){
+    incorrectNum++;
+    contentBuild();
+
+    $(".gameDiv").append($("<h3 class='text-center'>Incorrect! The answer is: "+correctArray[counter]+"</h3>"));
+
+    setTimeout(pause, 2000);
+};
+
+function pause(){
+    if (counter < 9){
+        counter++;
+        showQuestion();
+        timerStart = 30;
+        countDown();
+    } else {
+        gameOver();
+    }
+};
+
+function gameOver(){
+    contentBuild();
+
+    $(".gameDiv").append($("<h3>Correct Answers: "+ correctNum +"</h3>"));
+    $(".gameDiv").append($("<h3>Incorrect Answers: "+ incorrectNum +"</h3>"));
+    $(".gameDiv").append($("<h3>Unanswered Answers: "+ unAnswered +"</h3>"));
+    $(".gameDiv").append($("<button type='button' class='btn btn-secondary' id='reset'>Start Over?</button>"));
+
+};
+
+$("body").on("click", "#reset", function(){
+    
+    totalCorrect = 0;
+    incorrectNum = 0;
+    unAnswered = 0;
+    counter = 0;
+    time;
+    timerCounter = 30;
+    countDown();
+    showQuestion();
+});
